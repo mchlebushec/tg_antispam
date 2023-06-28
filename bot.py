d7ba2e0ff4b13c18a83a19c75ce3b76932454839
ne1rik <75090515+mchlebushec@users.noreply.github.com>
2023-06-28 18:32:00 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,7 +7,6 @@ bot = telebot.TeleBot("API_TOKEN")
 
 # Создание словаря для хранения сообщений
 messages = {}
-
 # Функция для подсчета количества сообщений
 def count_words(lst):
     counts = []
@@ -34,10 +33,10 @@ def echo_all(message):
         # Отправка сообщения пользователю о муте на час
         try:
             bot.restrict_chat_member(chat_id, user_id, until_date=time.time() + 3600)
+            bot.send_message(chat_id, "Пользователь @" + user + " был замучен на 1 час за спам, старайтесь больше не нарушать правила.")
+            bot.send_message(chat_id, "Первый мут на час, следующий мут будет на день")
         except telebot.apihelper.ApiTelegramException:
             print("Ошибка, я не могу замутить администратора.")
-        bot.send_message(chat_id, "Пользователь @" + user + " был замучен на 1 час за спам, старайтесь больше не нарушать правила.")
-        bot.send_message(chat_id, "Первый мут на час, следующий мут будет на день")
         # Отправка сообщения администратору о количестве сообщений пользователя
         try:
             bot.send_message(693188597, 'У пользователя с ником @' + user + " больше 4 сообщений '" + str(count_messages[count_messages.index(max(count_messages[1::2]))-1]) + "' за минуту, он был замучен.")
