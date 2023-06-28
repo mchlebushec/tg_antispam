3d4fc2659a83beac1a97f3a6462dd4ec2db95ebd
ne1rik <75090515+mchlebushec@users.noreply.github.com>
2023-06-28 18:42:48 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,12 +1,15 @@
-import telebot
 import time
 import string
+import asyncio
+from aiogram import Bot, Dispatcher, types
 
 # Создание объекта бота
-bot = telebot.TeleBot("API_TOKEN")
+bot = Bot(token="6106244196:AAH7abNC05562iga8Hf0nT4lcUBo3qddJpI")
+dp = Dispatcher(bot)
 
 # Создание словаря для хранения сообщений
 messages = {}
+
 # Функция для подсчета количества сообщений
 def count_words(lst):
     counts = []
@@ -19,8 +22,8 @@ def count_words(lst):
     return counts
 
 # Обработчик всех сообщений
-@bot.message_handler(func=lambda message: True)
-def echo_all(message):
+@dp.message_handler()
+async def echo_all(message: types.Message):
     user_id = message.from_user.id
     chat_id = message.chat.id
     user = message.from_user.username
@@ -32,16 +35,17 @@ def echo_all(message):
     if int(max(count_messages[1::2])) > 4:
         # Отправка сообщения пользователю о муте на час
         try:
-            bot.restrict_chat_member(chat_id, user_id, until_date=time.time() + 3600)
-            bot.send_message(chat_id, "Пользователь @" + user + " был замучен на 1 час за спам, старайтесь больше не нарушать правила.")
-            bot.send_message(chat_id, "Первый мут на час, следующий мут будет на день")
-        except telebot.apihelper.ApiTelegramException:
+            await bot.restrict_chat_member(chat_id, user_id, until_date=time.time() + 3600)
+            await bot.send_message(chat_id, "Пользователь @" + user + " был замучен на 1 час за спам, старайтесь больше не нарушать правила.")
+            await bot.send_message(chat_id, "Первый мут на час, следующий мут будет на день")
+        except Exception as e:
             print("Ошибка, я не могу замутить администратора.")
         # Отправка сообщения администратору о количестве сообщений пользователя
         try:
-            bot.send_message(693188597, 'У пользователя с ником @' + user + " больше 4 сообщений '" + str(count_messages[count_messages.index(max(count_messages[1::2]))-1]) + "' за минуту, он был замучен.")
-        except telebot.apihelper.ApiTelegramException:
+            await bot.send_message(693188597, 'У пользователя с ником @' + user + " больше 4 сообщений '" + str(count_messages[count_messages.index(max(count_messages[1::2]))-1]) + "' за минуту, он был замучен.")
+        except Exception as e:
             print("Не удалось отправить сообщение вам в ЛС, убедитесь что уже писали данному боту в личку.")
 
 # Запуск бота
-bot.polling(none_stop=True)
+if __name__ == '__main__':
+    asyncio.run(dp.start_polling(dp))
